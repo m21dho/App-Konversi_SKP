08f9fbb459ffd1c35c786b763a479789a88e7a99
2201020100IrfanIbrahim <2201020100@student.umrah.ac.id>
2024-11-09 11:29:09 +0700

    second commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,15 +33,19 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            
         </div>
+        @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
+        @endif
+        <a class="underline text-sm text-gray-600 hover:text-gray-900" style="padding:10px" href="{{ route('register') }}">
+            {{ __("don't have account??") }}
+        </a>
     </form>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SuperAdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::get('/dashboard', function () {
