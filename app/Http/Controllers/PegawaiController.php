420ff43a4436157c7ee0b4ba38f896fea1afc84a
MRidho <150924926+m21dho@users.noreply.github.com>
2024-11-11 22:10:50 +0700

    Update PegawaiController.php

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -2,7 +2,53 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Pegawai; // Impor model Pegawai
+use Illuminate\Http\Request;
+
+class PegawaiController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Pegawai::all();
+        return view('dashboard', compact('data'));
+    }
+
+    public function tambah(){
+        return view('tambah');
+    }
+
+    public function insert(Request $request)
+    {
+            // Validasi input jika diperlukan
+        $validated = $request->validate([
+            'pegawai' => 'required|string',
+            'penilai' => 'required|string',
+            'periodemulai' => 'required|date',
+            'periodeselesai' => 'required|date',
+        ]);
+
+        // Simpan data ke dalam database
+        Pegawai::create($validated);
+
+        // Redirect ke dashboard pegawai
+        return redirect()->route('dashboard');
+    }
+
+    public function tampildata($id){
+        $data = Pegawai::find($id);
+        return view('editdata', compact('data'));
+    }
+
+    public function updatedata(Request $request ,$id){
+        $data = Pegawai::find($id);
+        $data->update($request->all());
+        return redirect()->route('dashboard');
+    }
+
+    public function delete($id){
+        $data = Pegawai::find($id);
+        $data->delete();
+        return redirect()->route('dashboard');
+    }
+
 }
