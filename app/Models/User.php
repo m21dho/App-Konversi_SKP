983d2c686dd3f349b6d88d55d01aedda5f7ba799
MRidho <150924926+m21dho@users.noreply.github.com>
2024-11-15 15:25:11 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function pegawais()
+    {
+    return $this->hasMany(Pegawai::class);
+    }
 }
