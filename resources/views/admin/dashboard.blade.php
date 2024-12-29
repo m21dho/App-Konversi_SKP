e05d22d7bab16792ff806afbfab384da7d0de5d7
MRidho <150924926+m21dho@users.noreply.github.com>
2024-12-29 20:52:12 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,7 @@
-
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard Admin') }}
         </h2>
     </x-slot>
 
@@ -11,13 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                <style>
-                    table {
-                        border-collapse: collapse;
-                        width: 100%;
-                        border-radius: 10px;
-                        overflow: hidden; /* Agar sudut tabel tidak terpotong */
-                        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Tambahan bayangan untuk efek */
+                    @if (session('success'))
+                        <div style="position: fixed; top: 20px; right: 20px; background-color: #28a745; color: white; padding: 10px 20px; border-radius: 5px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); z-index: 1000;">
+                            <span onclick="this.parentElement.style.display='none';" style="cursor:pointer; float:right;">&times;</span>
+                            <strong>{{ session('success') }}</strong>
+                        </div>
+                    @endif
+                    <style>
+                        table {
+                            border-collapse: collapse;
+                            width: 100%;
+                            border-radius: 10px;
+                            overflow: hidden;
+                            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
                         }
                         th, td {
                             border: 1px solid #ddd;
@@ -30,32 +34,19 @@
                         tr:nth-child(even) {
                             background-color: #f9f9f9;
                         }
-                        .btn-upload {
+                        .btn {
+                            background-color: #060761;
+                            color: white;
+                            padding: 10px 20px;
+                            border: none;
+                            border-radius: 5px;
+                            cursor: pointer;
+                        }
+                        .btn:hover {
+                            background-color: #0d0fd9;
+                        }
+                        .btn-file {
                             background-color: #007bff;
-                            color: white;
-                            padding: 10px 20px;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                        }
-                        .btn-upload:hover {
-                            background-color: #0056b3;
-
-                        }
-                        .btn-delete {
-                            background-color: #cc0000;
-                            color: white;
-                            padding: 10px 20px;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                        }
-                        .btn-delete:hover {
-                            background-color: #7c0a02;
-
-                        }
-                        .btn-new {
-                            background-color: #12760b;
                             color: white;
                             padding: 10px 20px;
                             border: none;
@@ -63,67 +54,79 @@
                             cursor: pointer;
                             margin-bottom: 60px;
                         }
-                        .btn-new:hover {
-                            background-color: #17b80c;
+                        .btn-file:hover {
+                            background-color: #0056b3;
                         }
-                    h1{
-                        background-color: #12760b;
-                        padding: 10px 20px;
-                        border: none;
-                        border-radius: 5px;
-                        margin-bottom: 30px;
-                        text-align: center;
-                        font-weight: bold;
-                    }
-                </style>
-                <h1 class="h1">Data SKP</h1>
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">Nama Pegawai</th>
-                        <th scope="col">Nama Penilai</th>
-                        <th scope="col">Jenis Kegiatan</th>
-                        <th scope="col">Tanggal Submit</th>
-                        <th scope="col">File</th>
-                        <th scope="col">Koreksi</th>
-                        <th scope="col">Verifikasi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($data as $isi)
-                        <tr>
-                            <td>{{$isi ->pegawai}}</td>
-                            <td>{{$isi ->penilai}}</td>
-                            <td>
-                                @if ($isi->jenis_kegiatan == 'a')
-                                    Kegiatan A
-                                @elseif ($isi->jenis_kegiatan == 'b')
-                                    Kegiatan B
-                                @elseif ($isi->jenis_kegiatan == 'c')
-                                    Kegiatan C
-                                @else
-                                    Tidak Diketahui
-                                @endif
-                            </td>
-                            <td>{{$isi ->created_at}}</td>
-                            <td>
-                            <a href="{{ asset('storage/' . $isi->file) }}" target="_blank">
-                                {{ \Illuminate\Support\Str::limit($isi->file, 20) }} <!-- Batasi menjadi 20 karakter -->
-                            </a>
-                            </td>
-                            <td>
-                                <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
-                                    <a href="/admin/koreksi/{{ $isi->id }}" type="button" class="btn-upload">Koreksi</a>
-                                </div>
-                            </td>
-                            <td>
-                                <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
-                                    <a href="/tampildata/{{ $isi->id }}" type="button" class="btn-upload">Verifikasi</a>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
+                        h1 {
+                            background-color: #060761;
+                            border: 2px solid white;
+                            color: white;
+                            padding: 10px 20px;
+                            border-radius: 5px;
+                            margin-bottom: 30px;
+                            text-align: center;
+                            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                        }
+                        .notif {
+                            padding: 20px;
+                            color: white;
+                        }
+                    </style>
+                    <h1 class="h1">Data SKP</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nama Pegawai</th>
+                                <th scope="col">Nama Penilai</th>
+                                <th scope="col">Jenis Kegiatan</th>
+                                <th scope="col">Tanggal Submit</th>
+                                <th scope="col">File</th>
+                                <th scope="col">Koreksi</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Verifikasi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($data as $isi)
+                            <tr>
+                                <td>{{ $isi->pegawai }}</td>
+                                <td>{{ $isi->penilai }}</td>
+                                <td>
+                                    @if ($isi->jenis_kegiatan == 'a')
+                                        Kegiatan A
+                                    @elseif ($isi->jenis_kegiatan == 'b')
+                                        Kegiatan B
+                                    @elseif ($isi->jenis_kegiatan == 'c')
+                                        Kegiatan C
+                                    @else
+                                        Tidak Diketahui
+                                    @endif
+                                </td>
+                                <td>{{ $isi->created_at }}</td>
+                                <td>
+                                    <?php $filePath = asset('storage/' . $isi->file); ?>
+                                    <a href="{{ $filePath }}" target="_blank" class="btn-file">
+                                        FILE
+                                    </a>
+                                </td>
+                                <td>
+                                    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
+                                        <a href="{{ route('koreksi.show', $isi->id) }}" type="button" class="btn">Koreksi</a>
+                                    </div>
+                                </td>
+                                <td>{{ $isi->status }}</td>
+                                <td>
+                                    <div class="notif" style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
+                                        <form action="{{ route('admin.verifikasi', $isi->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="submit" class="btn">Verifikasi</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
